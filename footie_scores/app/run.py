f1c16819b3e2c5d8bf7e470893960bc349c69ef2
FredDavison <fred.dev99@gmail.com>
2017-04-19 21:09:47 +0100

    Add stylesheet

--- footie_scores/app/run.py
+++ footie_scores/app/run.py
@@ -21,21 +21,27 @@ def test():
 @app.route("/todays_games")
 def todays_fixtures():
     premier_league = FootballAPI(id_league='1204')
-    pl_games = premier_league.page_ready_todays_fixtures()
-    return games_template(pl_games, date.today())
+    champions_league = FootballAPI(id_league='1005')
+    #pl_games = premier_league.page_ready_todays_fixtures()
+    cl_games = champions_league.page_ready_todays_fixtures()
+    return games_template(cl_games, date.today())
 
 @app.route("/current_games")
 def active_fixtures():
-    premier_league = FootballAPI(id_league='1204')
-    pl_games = premier_league.page_ready_active_fixtures()
-    return games_template(pl_games, date.today())
+    # premier_league = FootballAPI(id_league='1204')
+    champions_league = FootballAPI(id_league='1005')
+    #pl_games = premier_league.page_ready_active_fixtures()
+    cl_games = champions_league.page_ready_active_fixtures()
+    return games_template(cl_games, date.today())
 
 @app.route("/past_games")
 def past_fixtures():
     date_ = date(year=2017, month=4, day=15)
     premier_league = FootballAPI(id_league='1204')
-    pl_games = premier_league.page_ready_finished_fixtures(date_)
-    return games_template(pl_games, date_)
+    champions_league = FootballAPI(id_league='1005')
+    #pl_games = premier_league.page_ready_finished_fixtures(date_)
+    cl_games = champions_league.page_ready_finished_fixtures(date_)
+    return games_template(cl_games, date_)
 
 
 def games_template(games, date_):
