d27a8d2c78bf85c91170de419eac2a501dbf1ecd
FredDavison <fred.dev99@gmail.com>
2017-04-28 21:55:24 +0100

    Use multithreading to separate webapp and api calls

--- footie_scores/start_app.py
+++ footie_scores/start_app.py
@@ -1,13 +1,20 @@
 ''' Runs the web app '''
 
+from multiprocessing import Process
+
 from footie_scores.utils.log import start_logging
 from footie_scores.app.run import app
+from footie_scores.league_manager import start_api_calls 
+from footie_scores.app import run
 
 
 def main():
     ''' Run web app '''
     start_logging()
-    app.run()
+    web_app = Process(target=app.run)
+    api_caller = Process(target=start_api_calls, args=(run.COMPETITIONS,))
+    web_app.start()
+    api_caller.start()
 
 
 if __name__ == '__main__':
