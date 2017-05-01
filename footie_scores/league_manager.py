877ffb450b9c5cf4507cce545ad6702e76bc2349
FredDavison <fred.dev99@gmail.com>
2017-05-01 20:36:41 +0100

    Function for getting todays fixtures once

--- footie_scores/league_manager.py
+++ footie_scores/league_manager.py
@@ -5,13 +5,13 @@ from collections import defaultdict
 
 from flask import Flask, render_template
 
-from footie_scores.app import run
+#from footie_scores.app import run
 from footie_scores.utils.cache import load_json
 from footie_scores.utils.log import start_logging
 from footie_scores.apis.football_api import FootballAPI
 from footie_scores.utils.scheduling import start_periodic_calls
 
-AVAILABLE_COMPETITIONS = set((
+AVAILABLE_COMPETITIONS = (
     'champions league',
     'europa league',
     'austria',
@@ -34,13 +34,18 @@ AVAILABLE_COMPETITIONS = set((
     'turkey',
     'ukraine',
     'russia',
-    ))
+    )
 
 
 def start_api_calls(competitions):
     comp_api_names = [c['api_name'] for c in competitions]
     comp_api = FootballAPI()
-    start_periodic_calls(30, comp_api.page_ready_todays_fixtures_to_db, (comp_api_names,))
+    start_periodic_calls(45, comp_api.page_ready_todays_fixtures_to_db, (comp_api_names,))
+
+
+def single_api_call():
+    comp_api = FootballAPI()
+    comp_api.page_ready_todays_fixtures_to_db(AVAILABLE_COMPETITIONS)
 
 
 def retrieve_fixtures_from_cache(competitions):
@@ -62,4 +67,4 @@ def retrieve_fixtures_from_cache(competitions):
 if __name__ == '__main__':
     start_logging()
 
-    start_api_calls(run.COMPETITIONS)
+    #start_api_calls(run.COMPETITIONS)
