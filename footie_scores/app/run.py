175c8763391b44b911f5961118e2c46e81fdd294
FredDavison <fred.dev99@gmail.com>
2017-05-29 11:18:20 +0100

    Pass datetime objects not formatted dates/times

--- footie_scores/app/run.py
+++ footie_scores/app/run.py
@@ -4,17 +4,16 @@ import datetime as dt
 
 from flask import Flask, render_template, request
 
-from footie_scores import settings
+from footie_scores import db, settings, utils
 from footie_scores.utils.log import start_logging
-from footie_scores import db
 from footie_scores.db import queries
 import footie_scores.league_manager as api_interface
 
 
 app = Flask(__name__)
 WEBDATEFORMAT = "%A %d %B %y" # e.g. Sunday 16 April 2017
-TODAY = dt.date.strftime(dt.date.today(), settings.DB_DATEFORMAT)
-YESTERDAY = dt.date.strftime(dt.date.today() - dt.timedelta(days=1), settings.DB_DATEFORMAT)
+TODAY = dt.date.strftime(utils.time.today(), settings.DB_DATEFORMAT)
+TODAY = utils.time.today()
 COMPS_FOR_PAGE = settings.COMPS
 
 
@@ -30,7 +29,7 @@ def todays_fixtures():
         comps = page_comps_only(queries.get_competitions(session))
         comp_ids = [c.api_id for c in comps]
         fixtures = queries.get_comp_grouped_fixtures_for_date(session, TODAY, comp_ids)
-        todays_games = games_template(fixtures, dt.date.today())
+        todays_games = games_template(fixtures, utils.time.today())
     return todays_games
 
 
