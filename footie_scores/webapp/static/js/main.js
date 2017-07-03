43c5deff1f0fb53bd373f4e218275a3b03007de3
FredDavison <fred.dev99@gmail.com>
2017-07-03 19:00:21 +0100

    Filter by competition on web page

--- footie_scores/webapp/static/js/main.js
+++ footie_scores/webapp/static/js/main.js
@@ -1,11 +1,31 @@
 function showSubList () {
-
+  var slideUp = $(this).next().css('display') === 'block';
+  console.log(slideUp);
+  $(".sub-list").slideUp(150);
+  if (slideUp) {
+    $(this).next().slideUp(150);
+  } else {
+    $(this).next().slideDown(150);
+  }
 }
 
+function filterCompetitions () {
+  $(".competition-container").hide();
+  var competition = $(this).text().replace(/ /g, "-");
+  console.log("#" + competition)
+  $("#" + competition).show();
+}
+
+function showAllCompetitions () {
+  $(".competition-container").show();
+  console.log("all comps pressed");
+}
+
+
+
 $( document ).ready(function() {
-  $(".sidebar strong").on("click", function(){
-    $(this).next().children(".sub-list").slideToggle(175);
-  })
-  
+  $(".sidebar strong").on("click", showSubList);
+  $(".sidebar .sub-list li.individual-competitions").on("click", filterCompetitions);
+  $(".sidebar .sub-list li#all-comps-button").on("click", showAllCompetitions);
 });
 
