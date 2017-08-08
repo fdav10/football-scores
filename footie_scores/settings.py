d5f02d864cc5118beb53b0a1ac155c9596c9994d
FredDavison <fred.dev99@gmail.com>
2017-08-08 18:45:56 +0200

    Echo sql output

--- footie_scores/settings.py
+++ footie_scores/settings.py
@@ -7,7 +7,7 @@ from footie_scores import constants
 logger = logging.getLogger(__name__)
 
 DB_PATH = os.environ['DATABASE_URL']
-SQLA_ECHO = False
+SQLA_ECHO = True
 DB_DATEFORMAT = '%d.%m.%Y'
 DB_TIMEFORMAT = '%H:%M'
 DB_DATETIMEFORMAT = DB_DATEFORMAT + '-' + DB_TIMEFORMAT
