dc3d01a99d2f5ac7bb7137ed7cc8e764182beffa
FredDavison <fred.dev99@gmail.com>
2017-08-10 14:02:50 +0200

    Sort date grouped fixtures by date

--- footie_scores/interfaces/db_to_web.py
+++ footie_scores/interfaces/db_to_web.py
@@ -19,8 +19,7 @@ TIME_OVERRIDE = settings.OVERRIDE_TIME or settings.OVERRIDE_DAY
 
 def get_fixture_by_id(session, id_):
     fixture = queries.get_fixture_by_id(session, id_)
-    if TIME_OVERRIDE:
-        fixture = filter_fixture_with_override_time(fixture)
+    fixture = filter_fixture_with_override_time(fixture)
     return fixture
 
 
@@ -32,8 +31,7 @@ def get_comp_grouped_fixtures(
     for id_ in comp_ids:
         competition = queries.get_competition_by_id(session, id_)
         fixtures = queries.get_fixtures_by_date_and_comp(session, start_date, id_, end_date)
-        if TIME_OVERRIDE:
-            fixtures = filter_fixtures_with_override_time(fixtures)
+        fixtures = filter_fixtures_with_override_time(fixtures)
         grouped_fixtures.append({'name': competition.name,
                                  'fixtures': fixtures})
     return grouped_fixtures
@@ -47,12 +45,13 @@ def get_date_grouped_fixtures(
     end_date = start_date if end_date is None else end_date
 
     fixtures = queries.get_fixtures_by_date_and_comp(session, start_date, comp_id, end_date)
-    if TIME_OVERRIDE:
-        fixtures = filter_fixtures_with_override_time(fixtures)
+    fixtures = filter_fixtures_with_override_time(fixtures)
     for fixture in fixtures:
         date_keyed_dict[fixture.date].append(fixture)
+    date_sorted_keys = sorted(list(date_keyed_dict.keys()))
 
-    for date, fixtures in date_keyed_dict.items():
+    for date in date_sorted_keys:
+        fixtures = date_keyed_dict[date]
         web_format_time = utils.time.custom_strftime(settings.WEB_DATEFORMAT, date)
         grouped_fixtures.append({'name': web_format_time,
                                  'fixtures': fixtures})
@@ -69,40 +68,44 @@ def get_competition_by_id(session, id_):
 
 
 def filter_fixtures_with_override_time(fixtures):
-    return [filter_fixture_with_override_time(f) for f in fixtures]
+    if TIME_OVERRIDE:
+        return [filter_fixture_with_override_time(f) for f in fixtures]
+    return fixtures
 
 
 def filter_fixture_with_override_time(fixture):
-    f = fixture
-    fixture_ko = dt.datetime.combine(f.date, f.time)
-    minutes_elapsed = (utils.time.now() - fixture_ko).total_seconds() / 60
-    gametime_elapsed = minutes_elapsed - (15 if minutes_elapsed > 45 else 0)
-    time_filtered_events = {'home': [], 'away': []}
+    if TIME_OVERRIDE:
+        f = fixture
+        fixture_ko = dt.datetime.combine(f.date, f.time)
+        minutes_elapsed = (utils.time.now() - fixture_ko).total_seconds() / 60
+        gametime_elapsed = minutes_elapsed - (15 if minutes_elapsed > 45 else 0)
+        time_filtered_events = {'home': [], 'away': []}
 
-    if gametime_elapsed < 0:
-        f.override_score = f.time.strftime(settings.DB_TIMEFORMAT)
-        f.override_events = time_filtered_events
-        f.override_status = ' '
-    else:
-        for team in ('home', 'away'):
-            for event in f.events[team]:
-                if gametime_elapsed >= int(event['time']):
-                    time_filtered_events[team].append(event)
-                else:
-                    logger.info('%s vs %s: %s at %s filtered, override game time: %s',
-                                f.team_home, f.team_away,
-                                event['type'], event['time'], gametime_elapsed)
-
-        if time_filtered_events != f.events or gametime_elapsed < 115:
+        if gametime_elapsed < 0:
+            f.override_score = f.time.strftime(settings.DB_TIMEFORMAT)
             f.override_events = time_filtered_events
-            home_goals = len([e for e in f.override_events['home'] if e['type'] == 'goal'])
-            away_goals = len([e for e in f.override_events['away'] if e['type'] == 'goal'])
-            f.override_score = ' - '.join((str(home_goals), str(away_goals)))
+            f.override_status = ' '
+        else:
+            for team in ('home', 'away'):
+                for event in f.events[team]:
+                    if gametime_elapsed >= event['minutes_since_ko']:
+                        time_filtered_events[team].append(event)
+                    else:
+                        logger.info('%s vs %s: %s at %s filtered, override game time: %s',
+                                    f.team_home, f.team_away,
+                                    event['type'], event['time'], gametime_elapsed)
 
-            # TODO this is unreliable, e.g. delayed games or games with ET
-            f.override_status = int(gametime_elapsed) if gametime_elapsed <= 115 else 'FT'
-            logger.info('%s vs %s: override score: %s, status: %s',
-                        f.team_home, f.team_away, f.override_score, f.override_status)
+            if time_filtered_events != f.events or gametime_elapsed < 115:
+                f.override_events = time_filtered_events
+                home_goals = len([e for e in f.override_events['home'] if e['type'] == 'goal'])
+                away_goals = len([e for e in f.override_events['away'] if e['type'] == 'goal'])
+                f.override_score = ' - '.join((str(home_goals), str(away_goals)))
+
+                # TODO this is unreliable, e.g. delayed games or games with ET
+                f.override_status = int(gametime_elapsed) if gametime_elapsed <= 115 else 'FT'
+                logger.info('%s vs %s: override score: %s, status: %s',
+                            f.team_home, f.team_away, f.override_score, f.override_status)
+        return fixture
     return fixture
 
 
