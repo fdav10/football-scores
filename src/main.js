356ff070bdc5c37ec66427df2905d06b01a53dd9
Dan Davison <dandavison7@gmail.com>
2017-08-10 17:56:10 +0200

    Remove react demo components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
-console.log('Hello World!');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
 
 document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
-  );
 });
