5cc22cc385932bc0d0ecda2a5f11e2f46ec47fad
Dan Davison <dandavison7@gmail.com>
2017-08-10 20:04:39 +0200

    Store score in component state

--- src/Score.js
+++ src/Score.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 class Score extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      score: ''
+    };
+  }
+
   render() {
     console.log('Score.render() for fixtureID = ' + this.props.fixtureID);
     return (
         <div className="score mui-col-xs-2">
-          <h4>{document.fixtureData[this.props.fixtureID].score}</h4>
+          <h4>{this.state.score}</h4>
         </div>
     )
   }
