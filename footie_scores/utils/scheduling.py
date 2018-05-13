6f667f4ed8422f0ff182ab6a5c99eb700c8500a9
FredDavison <fred.dev99@gmail.com>
2018-05-13 09:20:29 +0100

    Remove obsolete functions

--- footie_scores/utils/scheduling.py
+++ footie_scores/utils/scheduling.py
@@ -1,23 +1,4 @@
-import sched
 import grequests as gr
-
-SCHEDULER = sched.scheduler()
-
-
-def start_periodic_calls(interval, action, args=()):
-    s = sched.scheduler()
-    _periodic(s, interval, action, args)
-    s.run()
-
-
-def _periodic(scheduler, interval, action, args=()):
-    scheduler.enter(interval, 1, _periodic,
-                    (scheduler, interval, action, args))
-    action(*args)
-
-
-def schedule_action(action, time):
-    s = SCHEDULER.enter(time, 1, action)
 
 
 def batch_request(urls):
