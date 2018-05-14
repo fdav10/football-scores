7ea95068275c817d095bbe70d0bb778953f69655
FredDavison <fred.dev99@gmail.com>
2018-05-14 21:49:45 +0100

    Log requests to file

--- footie_scores/__init__.py
+++ footie_scores/__init__.py
@@ -2,3 +2,6 @@ import os
 
 PACKAGE_ROOT = os.path.split(__file__)[0]
 REPO_ROOT = os.path.join(PACKAGE_ROOT, os.pardir)
+
+if not os.path.exists(os.path.join(REPO_ROOT, 'logs')):
+    os.mkdir(os.path.join(REPO_ROOT, 'logs'))
