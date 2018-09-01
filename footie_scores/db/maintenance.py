d547fd0d0e0c1ca192afecf8ccd5a1f6a90e1b82
FredDavison <fred.dev99@gmail.com>
2018-09-01 09:37:14 +0200

    Save fetched fixtures to db during maintenance function

--- footie_scores/db/maintenance.py
+++ footie_scores/db/maintenance.py
@@ -4,8 +4,9 @@ import datetime as dt
 from footie_scores.db import session_scope
 from footie_scores.db import queries
 from footie_scores import settings, utils
-from footie_scores.apis.football_api import FootballAPI 
+from footie_scores.apis.football_api import FootballAPI
 from footie_scores.utils.log import start_logging
+from footie_scores.interfaces import api_to_db
 
 
 log_template = '''
@@ -59,17 +60,20 @@ def update_all_fixtures(competition_ids=settings.COMPS):
     Fixtures to be requested by competition and for a two month date
     range."""
 
-    api = FootballAPI()
+    with session_scope() as session:
+        api = FootballAPI()
 
-    start_date = dt.datetime.today() - dt.timedelta(days=200)
-    end_date = dt.datetime.today() + dt.timedelta(days=200)
-    start_dates = [start_date + dt.timedelta(days=60 * i) for i in range(6)]
-    end_dates = [date + dt.timedelta(days=60) for date in start_dates]
-    date_chunks = zip(start_dates, end_dates)
+        start_date = dt.datetime.today() - dt.timedelta(days=0)
+        end_date = dt.datetime.today() + dt.timedelta(days=30)
+        start_dates = [start_date + dt.timedelta(days=60 * i) for i in range(6)]
+        end_dates = [date + dt.timedelta(days=60) for date in start_dates]
+        date_chunks = zip(start_dates, end_dates)
 
-    for start_date, end_date in date_chunks:
-        api.get_fixtures_for_date(start_date, competition_ids,
-                                  end_date, split_requests=True)
+        for start_date, end_date in date_chunks:
+            fixtures = api.get_fixtures_for_date(
+                start_date, competition_ids, end_date,
+                split_requests=True)
+            api_to_db.save_fixtures(session, fixtures)
 
 
 if __name__ == '__main__':
