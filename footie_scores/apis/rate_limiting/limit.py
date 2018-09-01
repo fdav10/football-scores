8ea1c053c25306e8b443a2520bf6dc970111af43
FredDavison <fred.dev99@gmail.com>
2018-09-01 13:22:39 +0200

    Move log wrapper to utils

--- footie_scores/apis/rate_limiting/limit.py
+++ footie_scores/apis/rate_limiting/limit.py
@@ -23,5 +23,17 @@ def convert_log_to_table():
     import ipdb; ipdb.set_trace()
 
 
+def log_request(f, *args, **kwargs):
+    log_file_name = os.path.join(REPO_ROOT, 'logs', 'hourly_requests.log')
+    def requests_wrapper(instance, *urls, **kwargs):
+        log_urls = (urls,) if not isinstance(urls, (list, tuple)) else urls
+        with open(log_file_name, 'a') as log_file:
+            for url in log_urls:
+                log_file.write('{}\t{}\n'.format(instance.base_url + url, utils.time.now()))
+        output = f(instance, *urls, **kwargs)
+        return output
+    return requests_wrapper
+
+
 if __name__ == '__main__':
     convert_log_to_table()
