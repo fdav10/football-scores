d4be8404a6d2f6fd02d2bcb0ede677bbb6248cc1
FredDavison <fred.dev99@gmail.com>
2018-09-01 09:35:10 +0200

    Log when fixture not updated in db

--- footie_scores/interfaces/api_to_db.py
+++ footie_scores/interfaces/api_to_db.py
@@ -35,6 +35,8 @@ def save_fixture(session, api_fixture):
         existing_db_fixture = fq.filter(Fixture.api_fixture_id == db_fixture.api_fixture_id).first()
         if existing_db_fixture.update_from_equivalent(db_fixture):
             logger.info('%s updated in db', db_fixture)
+        else:
+            logger.info('%s already up to date in db', db_fixture)
 
 
 def save_lineups(session, api_lineups):
