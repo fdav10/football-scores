6619edd05a6d7b7aedf243cf03dfbefdf7c04fee
FredDavison <fred.dev99@gmail.com>
2018-09-02 11:49:53 +0200

    LogTable isn't list subclass

--- footie_scores/apis/rate_limiting/limit.py
+++ footie_scores/apis/rate_limiting/limit.py
@@ -12,11 +12,8 @@ from footie_scores import utils
 REQUESTS_LOG = os.path.join(REPO_ROOT, 'logs', 'hourly_requests.log')
 REQUESTS_RATE_LOG = os.path.join(REPO_ROOT, 'logs', 'requests_rate.log')
 
-_24_HRS_IN_SECONDS = 60 * 60 * 24
-_10_MINS_IN_SECONDS = 10 * 60
-
-SLEEP_TIME = _10_MINS_IN_SECONDS
-PURGE_OLDER_THAN = _24_HRS_IN_SECONDS
+SLEEP_TIME = 10 * 60 # ten minuts in seconds
+PURGE_OLDER_THAN = 60 * 60 # one hour in seconds
 
 TIME_FORMAT =  "%Y-%m-%d %H:%M:%S.%f%z"
 
@@ -30,28 +27,28 @@ class LogRecord:
     def __repr__(self):
         return f"<LogRecord url='{self.url}' time='{self.time}'>"
 
-class LogTable(list):
-    def __init__(self, items):
-        super(LogTable, self).__init__(items)
+class LogTable:
+    def __init__(self, records):
+        self._records = records
 
     def log_requests_rate(self):
         self._purge_old_records(older_than=PURGE_OLDER_THAN)
         self._update_requests_rate_log()
 
     def _purge_old_records(self, older_than):
-        ini_size = len(self)
-        self = [record for record in self
+        ini_size = len(self._records)
+        self._records = [record for record in self._records
                 if (dt.datetime.utcnow() - record.datetime).total_seconds()
-                < older_than]
+                <= older_than]
 
-        if ini_size != len(self):
-            print(f'hourly log reduced from {ini_size} to {len(self)}')
+        if ini_size != len(self._records):
+            print(f'hourly log reduced from {ini_size} to {len(self._records)}')
 
     def _update_requests_rate_log(self):
-        rate = str(len(self))
+        rate = str(len(self._records))
         time = dt.datetime.strftime(dt.datetime.utcnow(), TIME_FORMAT)
         with open(REQUESTS_RATE_LOG, 'a') as rate_file:
-            print('\t'.join([time, rate]) + ' requests per hour')
+            print('\t'.join([time, rate]) + ' requests per hour', flush=True)
             print('\t'.join([time, rate]), file=rate_file, end='\n')
 
 
@@ -63,6 +60,10 @@ def monitor_logs():
         table.log_requests_rate()
         print('sleeping for {:.0f} mins'.format(SLEEP_TIME / 60))
         sleep(SLEEP_TIME)
+
+
+def plot_request_rate():
+    pass
 
 
 if __name__ == '__main__':
